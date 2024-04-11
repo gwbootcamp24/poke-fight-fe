17b7abd1212a008b9718394702ea3bbaf0da4200
Milena Oswald <milena.oswald@icloud.com>
2024-04-11 21:56:08 +0200

    useContext für Hintergrund

--- src/pages/Startpage.jsx
+++ src/pages/Startpage.jsx
@@ -1,16 +1,25 @@
+import React, { useEffect } from "react";
+import { useBackgroundImage } from "../context/BackgroundImageContext.jsx";
 import Button from "../ui/Button.jsx";
 
 function Startpage() {
-  return (
-  <div className="startpage">      
+
+  const { setBackgroundImage } = useBackgroundImage();
+
+  useEffect(() => {
+    setBackgroundImage('https://ballonkunst-herzog.de/wp-content/uploads/2022/01/Ballons.png');
+  }, []);
+
+return (
+  <div>
     <div>
-        Das hier ist die Startpage mit dem Logo in der Mitte
+      Das hier ist die Startpage mit dem Logo in der Mitte
     </div>
     <div>
       <Button text="Start game" url="/lobby" />
     </div>
   </div>
-  );
+);
 }
- 
+
 export default Startpage;
