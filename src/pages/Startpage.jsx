9a8a06ef057d1ca9485b4a40dcaf1d5de1335fe6
Milena Oswald <milena.oswald@icloud.com>
2024-04-11 17:43:42 +0200

    css anfang

--- src/pages/Startpage.jsx
+++ src/pages/Startpage.jsx
@@ -2,14 +2,14 @@ import Button from "../ui/Button.jsx";
 
 function Startpage() {
   return (
-    <>
-      <div className="startpage">
+  <div className="startpage">      
+    <div>
         Das hier ist die Startpage mit dem Logo in der Mitte
-      </div>
-      <div>
-        <Button text="Start game" url="/lobby" />
-      </div>
-    </>
+    </div>
+    <div>
+      <Button text="Start game" url="/lobby" />
+    </div>
+  </div>
   );
 }
  
--- src/ui/PageLayout.jsx
+++ src/ui/PageLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function PageLayout() {
   return (
-    <div className="min-h-screen bg-green-200 " style={{ "width": "100vw", height: "100%", backgroundImage: "radial-gradient(circle, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)" }}>
+    <div className="pagelayout">
       <Outlet />
     </div>
   );
