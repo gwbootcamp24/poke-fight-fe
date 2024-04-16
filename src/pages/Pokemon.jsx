e4d71d3906148504e14d4aa74835c3413cefb8f2
Milena Oswald <milena.oswald@icloud.com>
2024-04-16 16:20:36 +0200

    details

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -31,19 +31,26 @@ function Pokemon() {
 
   return (
     <div className="m-2 mx-auto w-2/3 border-2 p-2 text-center">
-      <img className="title" src={Title}/>
-      <img className="yourPokemon" src={yourPokemon}/>
-      <Button className="leaderboard-btn" img={Leaderboard} url="/leaderboard" />
-      <img
-        alt={name}
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
-      />
-      <h2>{name}</h2>
-      <p>Type: {types.length > 0 && types.map(type => type.type.name).join(" | ")}</p>
-      <p>Abilities: {abilities.length > 0 && abilities.map(ability => ability.ability.name).join(", ")}</p>
-      <p>Stats: {stats.length > 0 && stats.map(stat => `${stat.base_stat} ${stat.stat.name}`).join(", ")}</p>
-      <Button img={Back} url="/lobby" />
-      <Button img={Fight} url={`/stareoff/${id}`} />
+      <div className="detail-menu">
+        <img className="title" src={Title}/>
+        <img className="yourPokemon" src={yourPokemon}/>
+        <Button className="leaderboard-btn" img={Leaderboard} url="/leaderboard"/>
+      </div>
+      <div className="pokemon-detail-grid">
+        <div className="stats">
+          <h2>Name: {name}</h2>
+          <p>Type: {types.length > 0 && types.map(type => type.type.name).join(" | ")}</p>
+          <p>Abilities: {abilities.length > 0 && abilities.map(ability => ability.ability.name).join(", ")}</p>
+          <p>Stats: {stats.length > 0 && stats.map(stat => `${stat.base_stat} ${stat.stat.name}`).join(", ")}</p>
+        </div>
+        <img className="pokemonId"
+          alt={name}
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+        />
+        <div className="emptyDiv"></div>
+      </div>
+      <Button className="btn-back" img={Back} url="/lobby" />
+      <Button className="btn-start" img={Fight} url={`/stareoff/${id}`} />
     </div>
   );
 }
