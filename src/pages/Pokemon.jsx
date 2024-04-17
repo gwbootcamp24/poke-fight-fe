e37824736f7f60ce857f776360793a8d4fe09c87
Milena Oswald <milena.oswald@icloud.com>
2024-04-17 09:34:07 +0200

    nichts

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,3 +1,64 @@
+// import { useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import { useFetch } from "../hooks/useFetch";
+// import { useBackgroundImage } from "../context/BackgroundImageContext.jsx";
+// import BackgroundImg from '../background/choose.png';
+// import Button from "../ui/Button.jsx";
+// import Leaderboard from '../font/leaderboard.png';
+// import Back from '../font/back.png';
+// import Fight from '../font/start.png';
+// import Title from '../font/pokefight_sm.png';
+// import yourPokemon from '../font/yourpokemon.png';
+
+// function Pokemon() {
+
+//   const { setBackgroundImage } = useBackgroundImage();
+
+//   useEffect(() => {
+//     setBackgroundImage(BackgroundImg);
+//   }, []);
+
+//   const { id } = useParams();
+//   const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  
+//   const [error, pokemonData] = useFetch(apiUrl);
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   const { name, types = [], abilities = [], stats = [] } = pokemonData;
+
+//   return (
+//     <div className="m-2 mx-auto w-2/3 border-2 p-2 text-center">
+//       <div className="detail-menu">
+//         <img className="title" src={Title}/>
+//         <img className="yourPokemon" src={yourPokemon}/>
+//         <Button className="leaderboard-btn" img={Leaderboard} url="/leaderboard"/>
+//       </div>
+//       <div className="pokemon-detail-grid">
+//         <div className="stats">
+//           <h2>Name: {name}</h2>
+//           <p>Type: {types.length > 0 && types.map(type => type.type.name).join(" | ")}</p>
+//           <p>Abilities: {abilities.length > 0 && abilities.map(ability => ability.ability.name).join(", ")}</p>
+//           <p>Stats: {stats.length > 0 && stats.map(stat => `${stat.base_stat} ${stat.stat.name}`).join(", ")}</p>
+//         </div>
+//         <img className="pokemonId"
+//           alt={name}
+//           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+//         />
+//         <div className="emptyDiv"></div>
+//       </div>
+//       <div>
+//        <Button className="btn-back" img={Back} url="/lobby" />
+//        <Button className="btn-start" img={Fight} url={`/stareoff/${id}`} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Pokemon;
+
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
@@ -19,9 +80,11 @@ function Pokemon() {
   }, []);
 
   const { id } = useParams();
-  const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
-  
+  // const apiUrl = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const apiUrl = `${import.meta.env.VITE_SERVER_URL}/pokemon/${id.toString()}`;
+  console.log(apiUrl)
   const [error, pokemonData] = useFetch(apiUrl);
+  console.log("pokemonData",pokemonData)
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -39,20 +102,23 @@ function Pokemon() {
       <div className="pokemon-detail-grid">
         <div className="stats">
           <h2>Name: {name}</h2>
-          <p>Type: {types.length > 0 && types.map(type => type.type.name).join(" | ")}</p>
-          <p>Abilities: {abilities.length > 0 && abilities.map(ability => ability.ability.name).join(", ")}</p>
-          <p>Stats: {stats.length > 0 && stats.map(stat => `${stat.base_stat} ${stat.stat.name}`).join(", ")}</p>
+          <p>Type: {types.map((type)=>(type + ', '))}</p>
+          <p>Abilities:{abilities.map((ability)=>(ability + ', '))}</p>
+          <p>Stats: ${stats}</p>
         </div>
         <img className="pokemonId"
           alt={name}
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+          src={`${import.meta.env.VITE_SERVER_URL}/sprites/pokemon/other/official-artwork/${id}.png`}
         />
         <div className="emptyDiv"></div>
       </div>
-      <Button className="btn-back" img={Back} url="/lobby" />
-      <Button className="btn-start" img={Fight} url={`/stareoff/${id}`} />
+      <div>
+       <Button className="btn-back" img={Back} url="/lobby" />
+       <Button className="btn-start" img={Fight} url={`/stareoff/${id}`} />
+      </div>
     </div>
   );
 }
 
 export default Pokemon;
+
--- src/pages/Stareoff.jsx
+++ src/pages/Stareoff.jsx
@@ -1,3 +1,92 @@
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import { useFetch } from "../hooks/useFetch.js";
+// import { useBackgroundImage } from "../context/BackgroundImageContext.jsx";
+// import Button from "../ui/Button.jsx";
+// import Leaderboard from '../font/leaderboard.png';
+
+// function Stareoff() {
+//   const { setBackgroundImage } = useBackgroundImage();
+
+//   useEffect(() => {
+//     setBackgroundImage('https://solarwissen.selfmade-energy.com/wp-content/uploads/2022/07/AdobeStock_216275201.jpeg');
+//   }, []);
+
+//   const { id: selectedPokemonId } = useParams();
+//   const selectedPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${selectedPokemonId}`;
+  
+//   const [selectedPokemon, setSelectedPokemon] = useState(null);
+//   const [randomPokemon, setRandomPokemon] = useState(null);
+
+//   // Daten des ausgewählten Pokemons abrufen
+//   useEffect(() => {
+//     const fetchSelectedPokemon = async () => {
+//       try {
+//         const res = await fetch(selectedPokemonUrl);
+//         if (!res.ok) {
+//           throw new Error("Failed to fetch selected Pokemon");
+//         }
+//         const data = await res.json();
+//         setSelectedPokemon(data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchSelectedPokemon();
+//   }, [selectedPokemonUrl]);
+
+//   // Zufälliges Pokemon generieren
+//   useEffect(() => {
+//     const randomId = Math.floor(Math.random() * 898) + 1; // Zufällige ID zwischen 1 und 898
+//     const randomPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+//     const fetchRandomPokemon = async () => {
+//       try {
+//         const res = await fetch(randomPokemonUrl);
+//         if (!res.ok) {
+//           throw new Error("Failed to fetch random Pokemon");
+//         }
+//         const data = await res.json();
+//         setRandomPokemon(data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     fetchRandomPokemon();
+//   }, []);
+
+//   // Wenn die Daten noch geladen werden, zeige "Loading..."
+//   if (!selectedPokemon || !randomPokemon) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="stareoff">
+//       <Button img={Leaderboard} url="/leaderboard" />
+//       <div className="selected-pokemon">
+//         <h2>Your Pokemon:</h2>
+//         <img
+//           alt={selectedPokemon.name}
+//           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${selectedPokemon.id}.png`}
+//         />
+//         <p>{selectedPokemon.name}</p>
+//       </div>
+//       <div className="random-pokemon">
+//         <h2>Your Opponent:</h2>
+//         <img
+//           alt={randomPokemon.name}
+//           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomPokemon.id}.png`}
+//         />
+//         <p>{randomPokemon.name}</p>
+//       </div>
+//       {/* Link zurück zur Pokemon-Komponente */}
+//       <Button text="Go back" url={`/pokemon/${selectedPokemonId}`} />
+//       <Button text="Fight!" url={`/fightarena/${selectedPokemonId}`} />
+//     </div>
+//   );
+// }
+
+// export default Stareoff;
+
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch.js";
@@ -13,47 +102,39 @@ function Stareoff() {
   }, []);
 
   const { id: selectedPokemonId } = useParams();
-  const selectedPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${selectedPokemonId}`;
-  
+  const selectedPokemonUrl = `${import.meta.env.VITE_SERVER_URL}/pokemon/${selectedPokemonId}`;
+  console.log(selectedPokemonUrl);
   const [selectedPokemon, setSelectedPokemon] = useState(null);
   const [randomPokemon, setRandomPokemon] = useState(null);
+  const [randomId, setRandomId] = useState(null);
+  
+  const [errorSelectedPokemon, pokemonData] = useFetch(selectedPokemonUrl);
+  console.log("pokemonData",pokemonData);
 
-  // Daten des ausgewählten Pokemons abrufen
-  useEffect(() => {
-    const fetchSelectedPokemon = async () => {
-      try {
-        const res = await fetch(selectedPokemonUrl);
-        if (!res.ok) {
-          throw new Error("Failed to fetch selected Pokemon");
-        }
-        const data = await res.json();
-        setSelectedPokemon(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchSelectedPokemon();
-  }, [selectedPokemonUrl]);
+
+  useEffect(()=>{
+    if (Object.keys(pokemonData).length > 0){
+      setSelectedPokemon(pokemonData);
+    }
+  }, [pokemonData]);
+
 
   // Zufälliges Pokemon generieren
-  useEffect(() => {
-    const randomId = Math.floor(Math.random() * 898) + 1; // Zufällige ID zwischen 1 und 898
-    const randomPokemonUrl = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
-    const fetchRandomPokemon = async () => {
-      try {
-        const res = await fetch(randomPokemonUrl);
-        if (!res.ok) {
-          throw new Error("Failed to fetch random Pokemon");
-        }
-        const data = await res.json();
-        setRandomPokemon(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchRandomPokemon();
+  useEffect(()=>{
+    const randomId = Math.floor(Math.random() * 1025) + 1; // Zufällige ID zwischen 1 und 1025
+    setRandomId(randomId)
   }, []);
 
+  const randomPokemonUrl = `${import.meta.env.VITE_SERVER_URL}/pokemon/${randomId}`;
+  const [errorRandomPokemon, randomPokemonData] = useFetch(randomPokemonUrl);
+
+  useEffect(()=>{
+    if (Object.keys(randomPokemonData).length > 0){
+      setRandomPokemon(randomPokemonData);
+    }
+  }, [randomPokemonData]);
+  
+    
   // Wenn die Daten noch geladen werden, zeige "Loading..."
   if (!selectedPokemon || !randomPokemon) {
     return <div>Loading...</div>;
@@ -66,7 +147,7 @@ function Stareoff() {
         <h2>Your Pokemon:</h2>
         <img
           alt={selectedPokemon.name}
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${selectedPokemon.id}.png`}
+          src={`${import.meta.env.VITE_SERVER_URL}/sprites/pokemon/other/official-artwork/${selectedPokemon.id}.png`}
         />
         <p>{selectedPokemon.name}</p>
       </div>
@@ -74,7 +155,7 @@ function Stareoff() {
         <h2>Your Opponent:</h2>
         <img
           alt={randomPokemon.name}
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomPokemon.id}.png`}
+          src={`${import.meta.env.VITE_SERVER_URL}/sprites/pokemon/other/official-artwork/${randomPokemon.id}.png`}
         />
         <p>{randomPokemon.name}</p>
       </div>
@@ -86,4 +167,5 @@ function Stareoff() {
 }
 
 export default Stareoff;
+
 
